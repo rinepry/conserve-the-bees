92567de0a23d660a3c4c32614836f1c417f0048d
rinepry <forestjm+data@pm.me>
2024-02-19 17:05:03 -0500

    clean family, genus and species tables

--- dataframe.sql
+++ dataframe.sql
@@ -1,18 +1,22 @@
 SELECT
-	gbifid_,
-	family_,
-	genus_,
-	scientificName_,
-	province_,
-	count_,
-	latitude_,
-	longitude_,
-	date_
+    gbifid_,
+    family_,
+    genus_,
+    species_,
+    infraspecificepithet_,
+    taxonrank_,
+    scientificname_,
+    verbatimscientificname_,
+    province_,
+    count_,
+    latitude_,
+    longitude_,
+    date_
 FROM
-	occurrences
+    occurrences
 WHERE
-	status_ = 'PRESENT' AND
-	date_ >= '1923-01-01 00:00:00' AND
-	date_ <= '2023-12-31 23:59:59'
+    status_ = 'PRESENT' AND
+    date_ >= '1923-01-01 00:00:00' AND
+    date_ <= '2023-12-31 23:59:59'
 ORDER BY
-	date_ ASC
+    date_ ASC
---
+++ fields.sql
@@ -0,0 +1,14 @@
+ALTER TABLE occurrences RENAME COLUMN "gbifID" TO gbifid_;
+ALTER TABLE occurrences RENAME COLUMN family TO family_;
+ALTER TABLE occurrences RENAME COLUMN genus TO genus_;
+ALTER TABLE occurrences RENAME COLUMN "species" TO species_;
+ALTER TABLE occurrences RENAME COLUMN "infraspecificEpithet" TO infraspecificepithet_;
+ALTER TABLE occurrences RENAME COLUMN "taxonRank" TO taxonrank_;
+ALTER TABLE occurrences RENAME COLUMN "scientificName" TO scientificname_;
+ALTER TABLE occurrences RENAME COLUMN "verbatimScientificName" TO verbatimscientificname_;
+ALTER TABLE occurrences RENAME COLUMN "stateProvince" TO province_;
+ALTER TABLE occurrences RENAME COLUMN "occurrenceStatus" TO status_;
+ALTER TABLE occurrences RENAME COLUMN "individualCount" TO count_;
+ALTER TABLE occurrences RENAME COLUMN "decimalLatitude" TO latitude_;
+ALTER TABLE occurrences RENAME COLUMN "decimalLongitude" TO longitude_;
+ALTER TABLE occurrences RENAME COLUMN "eventDate" TO date_;
